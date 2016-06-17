492611c1be70a0644de9a43796d3deb48a135408
romainberger <romain@romainberger.com>
2016-06-17 17:00:03 +0200

    Revert "Initial commit"
    This reverts commit 803ac4d1790b1de1d2b477561243740a872651a8.

--- test/index.js
+++ test/index.js
@@ -1,19 +1,7 @@
-import fs from 'fs'
-import path from 'path'
 import {expect} from 'chai'
-import diff from '../src'
 
-const sourcePath = path.join(__dirname, 'diff-source.css')
-const reversedPath = path.join(__dirname, 'diff-reversed.css')
-const resultPath = path.join(__dirname, 'diff-result.css')
-
-describe('Diff', () => {
-  it('should return only the diff', () => {
-    const source = fs.readFileSync(sourcePath, 'utf-8')
-    const reversed = fs.readFileSync(reversedPath, 'utf-8')
-    const expected = fs.readFileSync(resultPath, 'utf-8')
-
-    const output = diff(source, reversed)
-    expect(output).to.equal(expected)
+describe('', () => {
+  it('should do awesome shit', () => {
+    expect('Is it awesome?').to.equal('Hell yeah it is.')
   })
 })
